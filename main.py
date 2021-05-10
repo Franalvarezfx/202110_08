8824cb246624f7d3fdcf4aed2645d5f83feec599
pyplusplusMF <>
2021-05-10 01:47:28 +0000

    semana 1

--- main.py
+++ main.py
@@ -2,11 +2,15 @@
 Archivo principal de trabajo de todas las sesiones
 Recuerde que todos los archivos de trabajo se encuentran en la plataforma
 '''
+
+'''
+Ejercicios de la semana 1
+'''
 #import _08semana01clase01
+
 #import _08semana01clase02
-import _08semana01clase03
 
-
-
-
+#from _08semana01clase03.funciones import *
+#resultado = funcionSuma2 ( 20, 30)
+#print (resultado)
 
